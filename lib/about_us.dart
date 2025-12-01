5c64aaf80e2f179b58f911854c193cdd4530d88e
TimiOrejupe <107513334+TimiOrejupe@users.noreply.github.com>
2025-12-01 13:57:58 +0000

    Top of page sale banner added

--- lib/about_us.dart
+++ lib/about_us.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/material.dart';
 
+class AboutUsPage extends StatelessWidget {
+  const AboutUsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    void goHome() => Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
+
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // Top sale banner
+              Container(
+                color: const Color(0xFF4d2963),
+                padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+                child: const Text(
+                  'BIG SALE! OUR ESSENTIAL RANGE HAS DROPPED IN PRICE! OVER 20% OFF! COME GRAB YOURS WHILE STOCK LASTS!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white, fontSize: 14, fontWeight: FontWeight.w600),
+                ),
+              ),
+              
