22cba748c3e257f79ebf14854700813e704a5f52
TimiOrejupe <107513334+TimiOrejupe@users.noreply.github.com>
2025-12-02 22:24:29 +0000

    Import and salecollectionpage class added

---
+++ lib/sale_collection.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class SaleCollectionPage extends StatelessWidget {
+  const SaleCollectionPage({super.key});
+
+  static const _products = [
+    {
+      'title': 'Classic Sweatshirt',
+      'image': 'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
+      'price': '£35.00',
+      'sale': '£28.00',
+      'badge': '20% OFF'
+    },
+    {
+      'title': 'Campus Hoodie',
+      'image': 'https://shop.upsu.net/cdn/shop/files/PortsmouthCityPostcard2_1024x1024@2x.jpg?v=1752232561',
+      'price': '£45.00',
+      'sale': '£33.75',
+      'badge': '25% OFF'
+    },
+    {
+      'title': 'Branded T‑Shirt',
+      'image': 'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
+      'price': '£20.00',
+      'sale': '£14.00',
+      'badge': '30% OFF'
+    },
+    {
+      'title': 'Autumn Cap',
+      'image': 'https://shop.upsu.net/cdn/shop/files/PortsmouthCityPostcard2_1024x1024@2x.jpg?v=1752232561',
+      'price': '£12.00',
+      'sale': '£9.60',
+      'badge': '20% OFF'
+    },
+  ];
