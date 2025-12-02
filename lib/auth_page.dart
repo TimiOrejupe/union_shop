c46dd7a75bba5f07dea11ca5d5bb01553932e7e2
TimiOrejupe <107513334+TimiOrejupe@users.noreply.github.com>
2025-12-02 22:34:57 +0000

    Authpage class added

---
+++ lib/auth_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({super.key});
+
+  void _notImplemented(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Not implemented (demo)')));
+  }
