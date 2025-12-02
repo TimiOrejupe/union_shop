7e167c7db6cd394516a1dbf65f990dd010a47f8f
TimiOrejupe <107513334+TimiOrejupe@users.noreply.github.com>
2025-12-02 20:43:04 +0000

    Import and class added

---
+++ lib/autumn_collection.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AutumnCollectionPage extends StatelessWidget {
+  const AutumnCollectionPage({super.key});
+
+  static const _products = [
+    {'title': 'Autumn Hoodie', 'price': '£35.00', 'image': 'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282'},
+    {'title': 'Autumn T‑Shirt', 'price': '£18.00', 'image': 'https://shop.upsu.net/cdn/shop/files/PortsmouthCityPostcard2_1024x1024@2x.jpg?v=1752232561'},
+    {'title': 'Autumn Cap', 'price': '£12.00', 'image': 'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282'},
+    {'title': 'Autumn Scarf', 'price': '£10.00', 'image': 'https://shop.upsu.net/cdn/shop/files/PortsmouthCityPostcard2_1024x1024@2x.jpg?v=1752232561'},
+  ];
