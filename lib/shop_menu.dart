0e811c97a06a8c173aeaafc74aae099b9951de48
TimiOrejupe <107513334+TimiOrejupe@users.noreply.github.com>
2025-12-04 16:49:09 +0000

    ShopMenu class added

---
+++ lib/shop_menu.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ShopMenu extends StatelessWidget {
+  const ShopMenu({super.key});
+
+  static const _items = <String>[
+    'Clothing',
+    'Merchandise',
+    'Halloween 🎃',
+    'Signature & Essential Range',
+    'Portsmouth City Collection',
+    'Pride Collection 🏳️‍🌈',
+    'Graduation 🎓',
+  ];
